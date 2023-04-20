16c89e3176af8dacf69b8ed0cc3e398bafccf57f
johntk10 <jtkutbay@gmail.com>
2023-04-19 20:00:36 -0400

    commit

--- JKmain.py
+++ JKmain.py
@@ -1,35 +1,92 @@
-#Matthew Bianchi
+# John Kutbay
 
 import pygame
 from pygame.locals import *
-from pygame import mixer # for music if want
-from sys import exit
-from cards import cards
-import random
+import sys
+import time
+import functions
 
+def init():
+    global users
+    global dealers
+    global cards
+    users = []
+    dealers = []
+    cards = []
+    cards = functions.initcards(cards)
+    
 
-mixer.pre_init(44100, -16, -2, 2048) # need to look into what this does
 pygame.init()
+clock=pygame.time.Clock()
+X=1080
+Y=720
 
-screen = pygame.display.set_mode((720,480), HWSURFACE | DOUBLEBUF | RESIZABLE)
-pygame.display.set_caption('Blackjack')
-clock = pygame.time.Clock()
+# pygame window:
+functions.startWindow()
 
-background = pygame.image.load('graphics/table.png')
-background = pygame.transform.scale(background, screen.get_size())
-screen.blit(background,(0,0))
+# Intial text:
+functions.displayText('Welcome To Blackjack!', int(X/2), int(Y/5), int(X/20))
 
+# Initial Start Button:
+sbfont = pygame.font.SysFont('arialblack', int(X/16))
+sbcolor = (160,160,160)
+sbcolorL = (192,192,192)
+sbcolorD = (128,128,128)
+sbwidth = 250
+sbheight = 100
+sbtext = sbfont.render('Start', True, (255,255,255))
+
+pygame.display.flip()
+
+start = True
+game = False
 
 while True:
+    if start == True:
+        mouse = pygame.mouse.get_pos()
+        functions.startmouse(mouse, sbwidth, sbheight, sbcolor, sbcolorD, sbcolorL, sbtext)
+
+    if game == True:
+        # setting up and dealing 2 cards each
+        init()
+        users = functions.deal(2,1,users,cards)
+        dealers = functions.deal(2,2,dealers,cards)
+        
+
+
+        pygame.quit()
+        
+        # outputting the users cards and one of the dealers
+
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()
-        elif event.type == VIDEORESIZE:
-            screen = pygame.display.set_mode(event.dict['size'], HWSURFACE | DOUBLEBUF | RESIZABLE)
-            screen.blit(pygame.transform.scale(background, event.dict['size']), (0, 0))
-            pygame.display.flip()
-            
-            
+            quit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN and start == True:
+            if (X/2)-(sbwidth/2) <= mouse[0] <= (X/2)-(sbwidth/2)+sbwidth and (Y/2)-(sbheight/2) <= mouse[1] <= (Y/2)-(sbheight/2)+sbheight:
+                # change display after clicking button
+                start = False
+                game = True
+                functions.gameWindow()
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
     pygame.display.update()
-    clock.tick(60)
+    pygame.display.flip()
--- functions.py
+++ functions.py
@@ -1,100 +1,52 @@
-import graphics
-import time
 import pygame
 from pygame.locals import *
+import sys
 import random
+global screen
+X=1080
+Y=720
+pygame.init()
 
-Tcards = ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '10C', '10D', '10H', '10S', 'JC', 'JD', 'JH', 'JS', 'QC', 'QD', 'QH', 'QS', 'KC', 'KD', 'KH', 'KS', 'AC', 'AD', 'AH', 'AS',]
-
-# deals hand to user, adding the card and value to its class member
-def deal(UHand):
-  card = ""
-  while (card == ""):
-    card = random.choice(Tcards)
-    Tcards.remove(card)
+def startWindow():
+  global screen
+  screen = pygame.display.set_mode((1080,720), HWSURFACE | DOUBLEBUF)
+  background = pygame.image.load('graphics/table.png')
+  background = pygame.transform.scale(background, screen.get_size())
+  screen.blit(background, (0,0))
   
-  # checking cards to add values
-  UHand.add_cardF(card)
-  if int(card[0]) > 73 or card[0] == '1':
-    UHand.add_cardV(10)
-  elif int(card[0]) == 65:
-    UHand.add_cardV(11)
+def startmouse(mouse, sbwidth, sbheight, sbcolor, sbcolorD, sbcolorL, sbtext):
+  if (X/2)-(sbwidth/2) <= mouse[0] <= (X/2)-(sbwidth/2)+sbwidth and (Y/2)-(sbheight/2) <= mouse[1] <= (Y/2)-(sbheight/2)+sbheight:
+    pygame.draw.rect(screen, sbcolorL,[(X/2)-(sbwidth/2),(Y/2)-(sbheight/2),sbwidth,sbheight] ,0,7)
   else:
-    UHand.add_cardV(int(card[0]))
+    pygame.draw.rect(screen,sbcolorD,[(X/2)-(sbwidth/2),(Y/2)-(sbheight/2),sbwidth,sbheight],0,7)
+  screen.blit(sbtext, (X/2 - sbwidth/2 + 25, Y/2 - sbheight/2))
 
-# gets total of hand
-def total(UHand):
-  cards = UHand.get_cardV()
-  total = 0
-  for i in range(len(cards)):
-    total += cards[i]
+def gameWindow():
+  screen = pygame.display.set_mode((1080,720), HWSURFACE | DOUBLEBUF)
+  background = pygame.image.load('graphics/table.png')
+  background = pygame.transform.scale(background, screen.get_size())
+  screen.blit(background, (0,0))
+  pygame.display.flip()
 
-  return total
-
-# need to look into what this function does
-def draw(x, y, UHand):
-  new = nextCard(userHand)
-  graphics.draw_card(x, y, new)
-  x = x+72
-  return x
-
-# checks if ace in pack and swaps value to one
-def aceD(UHand, total):
-  cards = UHand.get_cardV()
-  if total > 21:
-    if 11 in cards:
-      index = cards.index(11)
-      cards[index] = 1
-      UHand.set_cardV(cards)
-
-# need to look into and possibly make better changes for self
-def dealerMove(UHand, x, y):
-  total = total(UHand)
-
-  while (total < 17):
-    deal(UHand)
-    x = draw(x, y, UHand)
-    total = total(UHand)
-    aceD(UHand, total)
-    total = total(UHand)
-    graphics.displayT(str(total), x, 13)
-    time.sleep(3)
-
-  return x
-
-# gets last card
-def lastCard(UHand):
-  tcards = UHand.get_cardF()
-  card = tcards[len(tcards)-1]
-  return card
+def displayText(text, x, y, size):
+  Font = pygame.font.SysFont('arialblack', size)
+  Text = Font.render(text, True, (255,255,255))
+  TextRect = Text.get_rect()
+  TextRect.center = (x,y)
+  screen.blit(Text,TextRect)
 
 
 
-"""
-def deal(num, player):
-  for i in range(0,num):
-    cardN = random.randint(0,9)
-    while cards[cardN] != 0:
-      cardN = random.randint(0,9)
-    cards[cardN] = player
-    if player == 1:
-      users.append(cardV[int(cardN % 13)] + cardS[int(cardN / 13)])
-    else:
-      dealers.append(cardV[int(cardN % 13)] + cardS[int(cardN / 13)])
-    
-def score(player):
-  scoreVal = 0
+def initcards(cards):
+  for i in range(0,51):
+    cards.append(0)
+  return cards
 
-  for i in range(52):
-    if cards[i] == player:
-      if (i%13) < 10 and (i%13) != 0:
-        scoreVal += (i%13)+1
-      elif (i%13) >= 10:
-        scoreVal += 10
-      else:
-        if (scoreVal + 11 > 21):
-          scoreVal += 1
-        else:
-          scoreVal += 11
-  return scoreVal
-"""
+def deal(num,player,pcards, cards): 
+  for card in range(0,num):
+    index = random.randint(0,51)
+    while cards[index] != 0:
+      index = random.randint(0,51)
+    cards[index] = player
+    pcards.append(index)
+  return pcards
